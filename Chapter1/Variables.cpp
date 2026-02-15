b7929e4d466af4ec64e11e1969ace81f285f7ddb
Diankes <122510601+Diankes@users.noreply.github.com>
2026-02-15 06:23:52 -0600

    Add Chapter 1.4: Variables, demonstrating output of literals in C++

---
+++ Chapter1/Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 5 << std::endl;       // print literal number `5`
+    std::cout << 11.2 << std::endl;    // print literal number `11.2`
+    std::cout << 'X' << std::endl;     // print literal character `X`
+    std::cout << "Hello" << std::endl; // print literal string `Hello`
+
+    return 0;
+}
