74885430dfccc85bc713ff689a12e0422cbeda0b
Diankes <122510601+Diankes@users.noreply.github.com>
2026-02-16 10:36:58 -0600

    Add functions.cpp: Implement a simple program demonstrating function usage with output

---
+++ Chapter2/functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void sayHello()
+{
+    std::cout << "Hello World" << std::endl;
+}
+
+int main()
+{
+    std::cout << "Looking through" << '\n';
+    sayHello();
+    std::cout << "functions" << '\n';
+    return 0;
+}
