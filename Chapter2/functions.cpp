ad78cba96e251e248ce77133562a3e068ef19a1a
Diankes <122510601+Diankes@users.noreply.github.com>
2026-02-16 20:51:11 -0600

    Simple implementation to explore void funtions and returning functions

--- Chapter2/functions.cpp
+++ Chapter2/functions.cpp
@@ -4,11 +4,19 @@ void sayHello()
 {
     std::cout << "Hello World" << std::endl;
 }
-
+int retrieveUserInput()
+{
+    int num{};
+    std::cin >> num;
+    return num;
+}
 int main()
 {
     std::cout << "Looking through" << '\n';
     sayHello();
     std::cout << "functions" << '\n';
+    std::cout << "Enter a number: " << '\n';
+    int n{retrieveUserInput()};
+    std::cout << "Your number times 3 is: " << n * 3 << std::endl;
     return 0;
 }
