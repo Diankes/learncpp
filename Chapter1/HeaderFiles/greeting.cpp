83422ec5f8014e39566d65cff1b40017d29e4152
Diankes <122510601+Diankes@users.noreply.github.com>
2026-02-17 16:26:36 -0600

    Simple header files usage implementation

---
+++ Chapter1/HeaderFiles/greeting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "greeting.h"
+
+// returns one
+int one()
+{
+    std::cout << "Hi from another module" << std::endl;
+    return 1;
+}
---
+++ Chapter1/HeaderFiles/main.cpp
@@ -0,0 +1,8 @@
+#include "greeting.h"
+#include <iostream>
+int main()
+{
+    int i = one();
+    std::cout << i << std::endl;
+    return 0;
+}
