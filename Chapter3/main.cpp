e53ca2f5f98e410148b724cdc77227cd849ecd35
Diankes <122510601+Diankes@users.noreply.github.com>
2026-02-28 22:43:38 -0600

    Add conditional preprocessor directives to enable debugging

--- Chapter3/main.cpp
+++ Chapter3/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#define ENABLE_DEBUG // Comment to disable debugging
 
 int getUserInteger()
 {
+#ifdef ENABLE_DEBUG
     std::cerr << "getUserInteger() called!\n";
+#endif
     std::cout << "Enter a number: ";
     int x{};
     std::cin >> x;
@@ -11,7 +14,9 @@ int getUserInteger()
 
 int main()
 {
+#ifdef ENABLE_DEBUG
     std::cerr << "main() called!\n";
+#endif
     int x{getUserInteger()};
     std::cout << "You entered: " << x << '\n';
 
